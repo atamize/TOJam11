330235559dac11b97a608746f9e63df9a6dfe2e3
Andrew Afan <atafan@gmail.com>
2016-05-06 20:33:42 -0400

    stuff

--- JamGame/Assets/Scripts/Main.cs
+++ JamGame/Assets/Scripts/Main.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SimpleAStarExample;
 
 public class Main : MonoBehaviour {
+    public Map map;
+    public List<Unit> units;
 
-	// Use this for initialization
-	void Start () {
-	
+    SearchParameters searchParams;
+
+	void Start()
+    {
+        map.InitTiles();
+        foreach (Unit unit in units)
+        {
+            var tile = map.GetTileWithState(unit.StartState);
+            unit.Tile = tile;
+            unit.transform.position = tile.worldPosition;
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(pos, Vector3.forward, out hit))
+            {
+                var to = hit.collider.GetComponent<TileObject>();
+                print("Hit " + to.x + ", " + to.y);
+
+                units[0].MoveTo(map, map.GetTile(to.x, to.y));
+            }
+        }
 	}
 }
---
+++ JamGame/Assets/Scripts/Unit.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleAStarExample;
+using DG.Tweening;
+
+public class Unit : MonoBehaviour {
+
+    public float speed;
+    public string startState;
+
+    public Tile Tile
+    {
+        get { return currentTile; }
+        set { currentTile = value; }
+    }
+
+    Transform mTransform;
+    Tile currentTile;
+    SearchParameters searchParams;
+
+    public char StartState
+    {
+        get { return startState[0]; }
+    }
+
+    void Start()
+    {
+        mTransform = this.transform;
+    }
+
+    public void MoveTo(Map map, Tile tile)
+    {
+        searchParams = new SearchParameters(currentTile.Location, tile.Location, map);
+        PathFinder pathFinder = new PathFinder(searchParams);
+        var path = pathFinder.FindPath();
+
+        StartCoroutine(Move(map, path));
+    }
+
+    IEnumerator Move(Map map, List<System.Drawing.Point> path)
+    {
+        foreach (var point in path)
+        {
+            Vector3 dest = map.GetTilePosition(point.X, point.Y);
+            yield return mTransform.DOMove(dest, speed).SetSpeedBased(true).SetEase(Ease.Linear).WaitForCompletion();
+        }
+    }
+}
